53a6622ed47a8f5f4805a0497497cc54279964dc
Shanuka-Peiris <80754567+Shanuka-Peiris@users.noreply.github.com>
2022-02-02 17:56:06 +0530

    Completed whatsup mern clone

---
+++ whatsapp-mern-clone/src/App.js
@@ -0,0 +1,46 @@
+import "./App.css";
+import Sidebar from "./Sidebar";
+import Chat from "./Chat";
+import React, { useEffect, useState } from "react";
+import Pusher from "pusher-js";
+import axios from "./axios";
+
+function App() {
+	const [messages, setMessages] = useState([]);
+
+	useEffect(() => {
+		axios.get("/messages/sync").then((response) => {
+			setMessages(response.data);
+		});
+	}, []);
+
+	useEffect(() => {
+		const pusher = new Pusher("f17cd9ccc6f8bd351052", {
+			cluster: "eu",
+		});
+
+		const channel = pusher.subscribe("messages");
+		channel.bind("inserted", (newMessage) => {
+			// alert(JSON.stringify(newMessage));
+			setMessages([...messages, newMessage]);
+		});
+
+		return () => {
+			channel.unbind_all();
+			channel.unsubscribe();
+		};
+	}, [messages]);
+
+	console.log("Messages", messages);
+
+	return (
+		<div className="app">
+			<div className="app__body">
+				<Sidebar />
+				<Chat messages={messages} />
+			</div>
+		</div>
+	);
+}
+
+export default App;
---
+++ whatsapp-mern-clone/src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+	apiKey: "AIzaSyDWNicDAwaBAmxtupPwP_TYleIbPMzSLZg",
+	authDomain: "whatsapp-mern-clone-cf469.firebaseapp.com",
+	projectId: "whatsapp-mern-clone-cf469",
+	storageBucket: "whatsapp-mern-clone-cf469.appspot.com",
+	messagingSenderId: "652264544495",
+	appId: "1:652264544495:web:757434343ec7326c32e31a",
+};
